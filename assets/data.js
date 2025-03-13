44099f2878e4edb3d76b4317efe972c9a54bc11b
Mithun <mithun.wys@gmail.com>
2025-03-13 12:30:46 +0530

    Added favicon

--- assets/data.js
+++ assets/data.js
@@ -4,6 +4,11 @@ const profileData = {
     profession: "QA Engineer & Web Developer",
     secondaryRole: "UI/UX Designer",
     profileImage: "data/Mithun.png", 
+    favicon: {
+        small: "data/favicon/dev-color-16.png",
+        default: "data/favicon/dev-color-32.png",
+        large: "data/favicon/dev-color-96.png"
+    },
     socialLinks: {
         linkedin: "https://www.linkedin.com/in/mithun-wijayasiri",
         github: "https://github.com/MithunWijayasiri",
